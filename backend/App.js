a566324f63d471004060b4301579419dff8ae935
Ibraheemkhan0078mn <ikkhan007m@gmail.com>
2025-01-03 17:56:01 +0500

    Update App.js

--- backend/App.js
+++ backend/App.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:[ process.env.VITE_Frontend_Base_Url ,  "https://ikchattingapp.netlify.app"],
+    origin: process.env.VITE_Frontend_Base_Url ||  "https://ikchattingapp.netlify.app",
     credentials:true
 }))
 
